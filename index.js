4a566836cad767134ace88706608e37e837ff4bf
sharatyaragatti <sharatyaragatti@credenceanalytics.com>
2024-11-10 12:37:48 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
+const cors = require('cors');
+const mongoose = require('mongoose');
+const path = require('path');
+const port = process.env.PORT || 8500;
+const connectString = process.env.URL;
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
